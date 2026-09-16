aad8d4dbf28339c52e823ac24ba8f1d02561a702
Easton <362585226@2260I-L29-032>
2026-09-16 12:44:44 -0400

    Added the basics

--- Topic-3-Variables-Assignment/Program.cs
+++ Topic-3-Variables-Assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            String firstname;
+            int age = 21, nextyearage;
+            nextyearage = age + 1;
+            double price;
+            price = 1.50;
+            const double Tax = 0.13;
+
+            firstname = "Bob";
+            Console.WriteLine("Hello " + firstname);
+            Console.WriteLine("You are " +  age);
+            Console.WriteLine("Next year you will be " + nextyearage);
+            Console.WriteLine("The price is " + (price * Tax).ToString("C"));
         }
     }
 }
