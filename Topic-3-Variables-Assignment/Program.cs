c0c76921ed6647c914132c73996e8b612a25f532
Easton <362585226@2260I-L29-032>
2026-09-16 12:21:39 -0400

    Add project files.

---
+++ Topic-3-Variables-Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Topic_3_Variables_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
