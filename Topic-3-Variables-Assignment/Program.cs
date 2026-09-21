22c07ffbc2e5432a00a617e81146db066e4e2789
Easton <362585226@2260I-L29-032>
2026-09-21 13:04:37 -0400

    Did parts 1 and 2 of assignment 3

--- Topic-3-Variables-Assignment/Program.cs
+++ Topic-3-Variables-Assignment/Program.cs
@@ -4,18 +4,41 @@
     {
         static void Main(string[] args)
         {
-            String firstname;
-            int age = 21, nextyearage;
-            nextyearage = age + 1;
-            double price;
-            price = 1.50;
-            const double Tax = 0.13;
+            //Console.WriteLine("This is just stuff from the lesson");
+            //int age = 21, nextyearage;
+            //nextyearage = age + 1;
+            //double price;
+            //price = 1.50;
+            //const double Tax = 0.13;
+            //Console.WriteLine("You are " +  age);
+            //Console.WriteLine("Next year you will be " + nextyearage);
+            //Console.WriteLine("The price is " + (price * Tax).ToString("C"));
+            //Console.WriteLine();
 
-            firstname = "Bob";
-            Console.WriteLine("Hello " + firstname);
-            Console.WriteLine("You are " +  age);
-            Console.WriteLine("Next year you will be " + nextyearage);
-            Console.WriteLine("The price is " + (price * Tax).ToString("C"));
+            int number = 29;
+            double price = 2.99;
+            string course = "Computer Science";
+
+
+            Console.WriteLine("This is room #" + number);
+            Console.WriteLine();
+            Console.WriteLine("The price is $" + price);
+            Console.WriteLine();
+            Console.WriteLine("I am learning a lot about " + course);
+            Console.WriteLine();
+
+            string firstname;
+            string lastname;
+            string year;
+
+            firstname = "Easton ";
+            lastname = "Powers ";
+            year = "2027";
+
+            Console.Write("My name is " + firstname + lastname); 
+            Console.WriteLine("and I graduate in " + year);
+
         }
     }
 }
+
